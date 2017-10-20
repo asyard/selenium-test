d3de0d263efe6c09e12e80b0bd2e52f2dd989f5a
asyard <asyard@gmail.com>
2017-10-20 09:15:18 +0300

    initial commits

--- src/main/java/com/asy/test/selenium/TestChrome.java
+++ src/main/java/com/asy/test/selenium/TestChrome.java
@@ -1,7 +1,12 @@
 package com.asy.test.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by asy
@@ -14,17 +19,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
  */
 public class TestChrome {
 
+    private static String userName = "";
+    private static String userPass = "";
+
     public static void main(String[] args) {
         //System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
         System.setProperty("webdriver.chrome.driver","C:\\selenium\\chromedriver_win32\\chromedriver.exe");
         WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("http://www.yahoo.com");
+        chromeDriver.get("http://www.gmail.com");
 
-        try {
-            Thread.sleep(20000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        new WebDriverWait(chromeDriver, 20).until(
+                webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+
+        WebDriverWait wait = new WebDriverWait(chromeDriver, 10);
+
+        WebElement userNameTextbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("identifierId")));
+        userNameTextbox.click();
+        userNameTextbox.sendKeys(userName);
+
+        chromeDriver.findElement(By.id("identifierNext")).click();
+
+        WebElement passwordTextbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        passwordTextbox.click();
+        passwordTextbox.sendKeys(userPass);
+
+        chromeDriver.findElement(By.id("passwordNext")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("T-I-KE")));
+
         chromeDriver.close();
     }
 
